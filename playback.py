2373a0c40c46921d58b7d97e36ba65b31e663e95
anyajiang0 <anyajiang0@gmail.com>
2025-07-21 12:56:25 -0400

    playback

---
+++ playback.py
@@ -0,0 +1,8 @@
+# ask for a string input 
+initialinput = input("Enter a phrase: ")
+
+# replace the spaces in the string with an ellipsis 
+outputstring = initialinput.replace(" ", "...")
+
+# print the output string
+print(outputstring)
